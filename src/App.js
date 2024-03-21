743ca90220a28e4a7e0b74491e311714d4af2b1d
noelalfonsomiranda <nafmiranda@unionbankph.com>
2024-03-21 18:32:05 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+import arrowIcon from "./arrow.svg";
+import data from "./data";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Our Pricing Plans</h1>
+      {/* 
+        Given the imported 'data'
+        Render the pricing plans here like the Design.png below
+      */}
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    title: "Beginner",
+    cost: 15,
+    features: [
+      "1000+ projects",
+      "No transaction fees",
+      "Unlimited Storage",
+      "5 Downloads"
+    ]
+  },
+  {
+    title: "Starter",
+    cost: 16,
+    features: [
+      "1000+ projects",
+      "No transaction fees",
+      "Unlimited Storage",
+      "5 Downloads"
+    ]
+  },
+  {
+    title: "Professional",
+    cost: 18,
+    features: [
+      "1000+ projects",
+      "No transaction fees",
+      "Unlimited Storage",
+      "5 Downloads"
+    ]
+  },
+  {
+    title: "Power Plan",
+    cost: 25,
+    features: [
+      "1000+ projects",
+      "No transaction fees",
+      "Unlimited Storage",
+      "5 Downloads"
+    ]
+  }
+];
